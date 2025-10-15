b74903e6e4fdd81341e8afc7a5beb391b83c1eac
GalactHD <139361517+GalactHD@users.noreply.github.com>
2025-10-15 11:27:45 -0300

    refactor: new spec for testing, and ci action

--- spec/gradientty_spec.cr
+++ spec/gradientty_spec.cr
@@ -2,28 +2,33 @@ require "spec"
 require "../src/gradientty"
 
 describe "Gradientty" do
-  it "prints predefined gradients correctly" do
+  it "generates ANSI-colored strings for predefined gradients" do
+    test_str = "Gradient Test"
+
+    gradients = {
+      "rainbow" => Gradientty.rainbow(test_str),
+      "crystal" => Gradientty.crystal(test_str),
+      "pastel"  => Gradientty.pastel(test_str),
+      "vice"    => Gradientty.vice(test_str),
+    }
+
+    gradients.each do |name, output|
+      output.should_not eq ""
+      # Check that ANSI escape codes exist
+      output.should match(/\e\[38;2;\d+;\d+;\d+m/)
+    end
+  end
+
+  it "generates ANSI-colored strings for custom gradients" do
     custom = Gradientty.gradient(["#ff5f6d", "#ffc371"])
+    output_1 = custom.call("CUSTOM")
+    output_2 = custom.multiline("LINE1\nLINE2", true)
 
-    output_0 = Gradientty.rainbow("A test for testing string with gradient.")
-    output_1 = Gradientty.crystal("A test for testing string with gradient.")
-    output_2 = Gradientty.pastel("A test for testing string with gradient.")
-    output_3 = Gradientty.vice("A test for testing string with gradient.")
-    output_4 = custom.call("CUSTOM")
-    output_5 = custom.multiline("LINE1\nLINE2")
-
-    output_0.should_not eq ""
     output_1.should_not eq ""
     output_2.should_not eq ""
-    output_3.should_not eq ""
-    output_4.should_not eq ""
-    output_5.should_not eq ""
 
-    puts output_0
-    puts output_1
-    puts output_2
-    puts output_3
-    puts output_4
-    puts output_5
+    # Check that ANSI escape codes exist
+    output_1.should match(/\e\[38;2;\d+;\d+;\d+m/)
+    output_2.should match(/\e\[38;2;\d+;\d+;\d+m/)
   end
 end
